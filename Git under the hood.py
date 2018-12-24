b5e428ed881b0b09037d2a3d08bf160ed3588250
aminosninatos <christopherfgreen@hotmail.com>
2018-12-24 13:36:38 +0100

    Initial commit

---
+++ Git under the hood.py
@@ -0,0 +1,20 @@
+Git under the hood
+-----------------------
+
+Verify a pack
+-----------------------------------------------------
+> git verify-pack -v filename.pack
+to get just the first filed :
+> git verify-pack -v filename.pack | awk '{print $1}'
+
+
+Unpack a file
+-----------------------------------------------------
+> git unpack-objects filename.pack
+you must unpack the file outside the .git directory
+
+
+Count command in git
+----------------------------------
+> ls /usr/lib/git-core | wc -l
+
