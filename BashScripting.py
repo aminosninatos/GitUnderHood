644416b58301df830bf7368f3c2dbc216a5e4d78
aminosninatos <christopherfgreen@hotmail.com>
2019-01-15 14:50:14 +0100

    Add shebang exit status and path tips to BashScripting.py

---
+++ BashScripting.py
@@ -0,0 +1,21 @@
+Bash scripting
+-------------------
+
+
+Shebang
+-------------------------------------------------------------
+#!/bin/bash
+the nterpreter that is going to be used to execute the script
+here '#' is not a comment in this case
+
+Exit status
+-------------------------------------------
+# echo $? 
+to get the exit status of the last command
+
+Path variable
+-------------------------------------------------------------------------
+on linux the current directory is not in the $PATH so to execute a script
+# ./mysript
+
+
