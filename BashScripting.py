f36d3d37f7d09495c237d764f57fdc6f298363bd
aminosninatos <christopherfgreen@hotmail.com>
2019-01-16 10:40:52 +0100

    Add stderr and stdout tips to BashScripting.py

--- BashScripting.py
+++ BashScripting.py
@@ -16,6 +16,14 @@ to get the exit status of the last command
 Path variable
 -------------------------------------------------------------------------
 on linux the current directory is not in the $PATH so to execute a script
-# ./mysript
+# ./myscript
 
+
+2>&1  expression
+---------------------------------------------------------------------------------
+strace prints its traces on standard error, not on standard output. 
+thats because its common to want to redirect the standard output of the program.
+but usually not a problem that strace stderr and the programs stderr are mixed.
+so you should redirect strace stderr to stdout to be able to pipe it:
+# sudo strace -p $(pgrep apache2) 2>&1 | grep open
 
