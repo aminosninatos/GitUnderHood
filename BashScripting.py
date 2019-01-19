05978aa5617498c18a7388f8fdfa6df588995744
aminosninatos <christopherfgreen@hotmail.com>
2019-01-19 20:11:15 +0100

    Add fix typos and spaces to BashScripting.py

--- BashScripting.py
+++ BashScripting.py
@@ -3,25 +3,25 @@ Bash scripting
 
 
 Shebang
--------------------------------------------------------------
+------------------------------------------------------------------
 #!/bin/bash
-the nterpreter that is going to be used to execute the script
-here '#' is not a comment in this case
+tells which interpreter is going to be used to execute the script.
+here '#' is not a comment in this case.
 
 Exit status
 -------------------------------------------
 > echo $? 
-to get the exit status of the last command
+to get the exit status of the last command.
 
 Path variable
 -------------------------------------------------------------------------
-on linux the current directory is not in the $PATH so to execute a script
+on linux the current directory is not in the $PATH so to execute a script:
 > ./myscript
 
 
 2>&1  expression
 ---------------------------------------------------------------------------------
-strace prints its traces on standard error, not on standard output. 
+"strace" prints its traces on standard error, not on standard output. 
 thats because its common to want to redirect the standard output of the program.
 but usually not a problem that strace stderr and the programs stderr are mixed.
 so you should redirect strace stderr to stdout to be able to pipe it:
@@ -33,20 +33,20 @@ Bash -x
 if you want your script to show every line before executing it.
 
 Variable Shell and SubShell
--------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------
 a subshell is created whenever you execute a script.
-a variable is oonly effective in the shell  when it was defined.
-we use EXPORT to make it also available in subshells.
-/etc/profile is processed when OPENING a LOGIN SHELL-user specific version is on ~/.bash_profile.
-/etc/bashrc  is processed when OPENING a SUBSHELL -user specific version is on ~/.bashrc.
+a variable is only effective in the shell when it was defined.
+we use "EXPORT" to make it also available in subshells.
+/etc/profile is processed when OPENING a LOGIN SHELL - user specific version is on ~/.bash_profile.
+/etc/bashrc is processed when OPENING a SUBSHELL - user specific version is on ~/.bashrc.
 
 Sourcing
--------------------------------------------------------------------------------
+---------------------------------------------------------------------
 the content of one script can be included in another script.
 we use the "source" command or the "." command to source a script.
 
 Arguments
------------------------------------------------------------------
+----------------------------------------------------------------
 "$0" refers to the name of the script itself.
 "$1" "$2" $... refer to the first second .... argument.
 "$@" refers to all arguments provided one by one.
@@ -54,13 +54,13 @@ Arguments
 "$*" refers to a single string that contains all arguments.
 
 Command substitution
-------------------------------------------------------------------------------
+-----------------------------------------------------------------------
 allows using the result of a command in a script by using the "$" sign.
 > ls -l $(which passwd)
 
 substitution operator
------------------------------------------------------------------------------------
-${VAR:-word} if $var exists, use its value if not return the value "word".
+--------------------------------------------------------------------------------------
+${VAR:-word} if $var exists, use its value if not display the value "word".
 ${VAR:=word} if $var exists, use its value if not set the default value to "word".
 ${VAR:?message} if $var exists, use its value if not display var followed by a message.
 
