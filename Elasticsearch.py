8e7cc65d5ff8ae74f6d33c944a7d69f2ca6a29a5
aminosninatos <christopherfgreen@hotmail.com>
2019-03-21 14:28:39 +0100

    Add data types tips to Elasticsearch.py

--- Elasticsearch.py
+++ Elasticsearch.py
@@ -4,7 +4,18 @@ Elasticsearch
 
 Introduction
 --------------------
-is Nosql distributed full-text database.
+Is Nosql distributed full-text database.
 Elasticsearch stores data in documents of json strings & the schema is not required. 
 Elasticsearch slices index into multiple shards. so they can be stored across multiple servers.
 
+Elasticsearch schema
+-----------------------------------------------------------
+Elasticsearch has schema called mapping.
+To get a list of indices:  GET /_cat/indices
+To get an index mapping: GET index/_mapping
+To get all documents in an index: GET index/type/_search
+
+
+Types
+------------------------------------------------------------------------
+Elasticsearch has 5 data types: String,Boolean, Numbers, Date & Binary 
