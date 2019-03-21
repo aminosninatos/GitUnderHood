dd100392f762acced261d5418951123e9e6969c8
aminosninatos <christopherfgreen@hotmail.com>
2019-03-21 11:17:30 +0100

    Add Introduction tips to Elasticsearch.py

---
+++ Elasticsearch.py
@@ -0,0 +1,10 @@
+Elasticsearch
+----------------------
+----------------------
+
+Introduction
+--------------------
+is Nosql distributed full-text database.
+Elasticsearch stores data in documents of json strings & the schema is not required. 
+Elasticsearch slices index into multiple shards. so they can be stored across multiple servers.
+
