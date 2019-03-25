53c533a52e6a07c960542af675b3c15d19ab4e2a
aminosninatos <christopherfgreen@hotmail.com>
2019-03-25 15:12:34 +0100

    Add topics theory to Kafka.py

--- Kafka.py
+++ Kafka.py
@@ -4,3 +4,12 @@ Kafka
 
 
 Source system(s) ----------DATA------------->Target system(s)
+
+
+Kafka theory
+----------------------------------------------------------------------
+Topics: is a particular stream of DATA (similar to table in database).
+Topics are split into partitions.
+Each message within a partition get an incremantal id called offeset.
+Data is only kept a limited time (default is a week).
+
