244c5247bddcc741ef894c5effbbc315e27a7a18
aminosninatos <christopherfgreen@hotmail.com>
2019-03-25 16:10:18 +0100

    Add brockers tips theory to Kafka.py

--- Kafka.py
+++ Kafka.py
@@ -13,3 +13,11 @@ Topics are split into partitions.
 Each message within a partition get an incremantal id called offeset.
 Data is only kept a limited time (default is a week).
 
+Brockers
+---------------------------------------------------------------------
+Kafka cluster is composed of many brockers(servers).
+Brocker contains certain topic partitions because kafka is distributed.
+Topics should have a replication factor > 1
+Each partition have one leader brocker and multiple ISR(in-syc-replica).
+Zookeeper decides who is the leader and ISR.
+
