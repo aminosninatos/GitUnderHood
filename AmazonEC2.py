d680f5c99a4628837882bf5bfdb24784a3d67b43
aminosninatos <christopherfgreen@hotmail.com>
2019-05-06 23:19:52 +0200

    Add aws EC2 instance tips to AmazonEC2.py

--- AmazonEC2.py
+++ AmazonEC2.py
@@ -7,8 +7,16 @@ AWS is a cloud provider.
 EC2 is the main service to provide machines on demand.
 
 EC2 basics
--------------------------------------------------------
+--------------------------------------------------------------------
 Renting virtual machines(EC2).
 Storing data on virtual drives(EBS).
 Distruibuting load across machines(ELB).
 Scaling the services using auto scaling groups(ASG).
+
+EC2 instances
+---------------------------------------------------------------------------------------------------
+You will pay for instances that are running so stop an instance to save money.
+When you terminate an instance it is completely removed and all data on it.
+To use the private key file to access the instance : > chmod 400 keyfile.pem
+
+
