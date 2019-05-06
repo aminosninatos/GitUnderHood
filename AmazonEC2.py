f8569fd8db88785c63c354c553911c18863b4397
aminosninatos <christopherfgreen@hotmail.com>
2019-05-06 22:56:37 +0200

    Add intro to aws EC2 basics to AmazonEC2.py

---
+++ AmazonEC2.py
@@ -0,0 +1,14 @@
+AMAZON EC2
+------------------
+------------------
+
+
+AWS is a cloud provider.
+EC2 is the main service to provide machines on demand.
+
+EC2 basics
+-------------------------------------------------------
+Renting virtual machines(EC2).
+Storing data on virtual drives(EBS).
+Distruibuting load across machines(ELB).
+Scaling the services using auto scaling groups(ASG).
