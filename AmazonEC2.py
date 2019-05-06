cb81fd359594bce63a5ef2b2be2370734ebedb25
aminosninatos <christopherfgreen@hotmail.com>
2019-05-06 23:53:04 +0200

    Add aws EC2 security groups tips to AmazonEC2.py

--- AmazonEC2.py
+++ AmazonEC2.py
@@ -19,4 +19,11 @@ You will pay for instances that are running so stop an instance to save money.
 When you terminate an instance it is completely removed and all data on it.
 To use the private key file to access the instance : > chmod 400 keyfile.pem
 
-
+Security groups
+---------------------------------------------------------------------------------------------------------------
+Are acting as a firewall  on EC2 instances.
+They regulate access ports, authorized IP ranges & control inbound and outbound trafiic.
+One security group can be attached to multiple instaces.
+Locked down to a region /vpv combination.
+If your app is not accessible (time out) then its a security group issue.
+If your app gives (connection refused )error then its an app error or its not launched. 
