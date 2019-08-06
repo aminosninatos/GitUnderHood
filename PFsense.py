4d2d2f01074a0761346f2a320967290e30815fbc
aminosninatos <christopherfgreen@hotmail.com>
2019-08-06 12:04:39 +0100

    Add OpenVPN steps tips to PFsense.py

---
+++ PFsense.py
@@ -0,0 +1,14 @@
+PFsense Tips and Tricks
+----------------------------
+----------------------------
+
+
+OpenVPN Steps
+--------------------------------------------------------------
+1- Create a Certificate Authority on PFsense (CA).
+2- Create an OpenVPN Server certificate.
+3- Configure an OpenVPN Server on PFsense.
+4- Configure an OpenVPN Client Access on PFSense.
+5- Install the OpenVPN Client Export Package.
+6- Add the VPN User.
+7- Download the Client Configuration using to Client Export. 
