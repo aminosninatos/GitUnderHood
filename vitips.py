2b06cb169094164bc06b3048077f79a6712cb5ad
aminosninatos <christopherfgreen@hotmail.com>
2019-08-13 20:21:11 +0100

    Add new file vitips.py

---
+++ vitips.py
@@ -0,0 +1,22 @@
+Clipboard
+-----------------------
+dw	delete word
+dd	delete line
+p	put after cursor
+P	put before cursor
+yw	copy word
+yy	copy line
+
+Moves
+-------------------------------------------------------
+$  : moves the cursor to the end of the line
+0  : moves the cursor to the beginning of the line
+Shift + g : moves to the end of the file
+gg : moves to the beginning of the file
+
+Search
+------------------------------------------------------
+/pattern : searches for a pattern
+?pattern : searches backward for a pattern
+n : repeat the search in the same direction
+
