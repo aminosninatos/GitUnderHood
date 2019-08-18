d38317224358d4312473e603241be502fde9c2b7
aminosninatos <christopherfgreen@hotmail.com>
2019-08-18 12:40:42 +0100

    Add Python environment setup to Pythontips.py

---
+++ Pythontips.py
@@ -0,0 +1,6 @@
+Python Virtual environments
+----------------------------------------------------------
+1- To use it: #python3 -m venv Project_Directory
+2- To activate it: #source /Project_Directory/bin/activate
+3- To desactivate it: #deactivate 
+4- To show packages with version in your env: #pip list
