e4478f795827ca6dddee425614cf8741bc394f3b
aminosninatos <christopherfgreen@hotmail.com>
2019-08-20 11:02:27 +0100

    Add virtual environment & vs code tips to Pythontips.py

--- Pythontips.py
+++ Pythontips.py
@@ -1,6 +1,10 @@
 Python Virtual environments
-----------------------------------------------------------
-1- To use it: #python3 -m venv Project_Directory
-2- To activate it: #source /Project_Directory/bin/activate
-3- To desactivate it: #deactivate 
-4- To show packages with version in your env: #pip list
+---------------------------------------------------------------------------------
+In your project direcoty we gonna create a folder env:
+1- To use it: ~> python3 -m venv env
+2- To activate it: ~> source env/bin/activate
+3- To desactivate it: ~> deactivate 
+4- To show packages with version in your env: ~> pip list
+To work with that in vs code all you have to do is to reload your vs code 
+then click in the botton left the python version and choose the one in env folder.
+
