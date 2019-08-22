872a0fc3ab644b3a652f787159b8f6d9ea9e9c96
aminosninatos <christopherfgreen@hotmail.com>
2019-08-22 10:28:12 +0100

    Add pipenv setup & tips to Pythontips.py

--- Pythontips.py
+++ Pythontips.py
@@ -8,3 +8,17 @@ In your project direcoty we gonna create a folder env:
 To work with that in vs code all you have to do is to reload your vs code 
 then click in the botton left the python version and choose the one in env folder.
 
+
+Pipenv
+-----------------------------------------------------
+the new way to use virtual environments
+- Install pipenv: ~>pip3 install pipenv
+- Activate: ~>pipenv shell
+- Check version of Python:  ~>python --version
+- Install a package: ~>pipenv install package_name
+- Check local packages: ~>pipenv lock -r
+- Uninstall a package: ~>pipenv uninstall package_name
+- Install a dev package: ~>pipenv install package_name --dev
+
+
+
