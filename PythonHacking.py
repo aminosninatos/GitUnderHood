9b8224235571efdebb67d9956aaf00182a0c89fd
aminosninatos <christopherfgreen@hotmail.com>
2019-10-02 20:08:32 +0100

    Add call function of subprocess module desc

--- PythonHacking.py
+++ PythonHacking.py
@@ -1,3 +1,10 @@
 Atom shortcuts
 -----------------------------------------
 To comment & uncomment we use : Cmd + /
+
+Subprocess Module
+--------------------
+Using the call function of this module we can execute commands.
+subprocess.call("Your_command").
+to avoid hijacking code we can use:
+subprocess.call(["command","argument1","argument2"])
