ed7792092f6af862860f3c0bb7a5acaee463b412
aminosninatos <christopherfgreen@hotmail.com>
2019-10-24 21:07:08 +0100

    Add formatting string tips to PythonBasics.py

--- PythonBasics.py
+++ PythonBasics.py
@@ -3,3 +3,12 @@ Divison in python
 whenever there is a divison python will throw a floating number x.y
 to get an integer divison we use the // operator.
 to get the reminder we use the module operator %.
+
+Formating string
+-----------------
+in python 3.6 we concatenate string Using f string:
+str1 = 'x'
+mystr = f'hello, {str1}'
+in python 3.5 and below we can use:
+mystr0 = 'hello, {}'
+mystr = 'hello, '.format(str1)
