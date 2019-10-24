d210f1d9151123a33379679794c523a421fe0034
aminosninatos <christopherfgreen@hotmail.com>
2019-10-24 20:50:39 +0100

    Add division operator tips to PythonBasics.py

---
+++ PythonBasics.py
@@ -0,0 +1,5 @@
+Divison in python
+------------------
+whenever there is a divison python will throw a floating number x.y
+to get an integer divison we use the // operator.
+to get the reminder we use the module operator %.
