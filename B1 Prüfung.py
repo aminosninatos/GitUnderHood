0cec7878d81a738060441faa48d5838d1ed48477
Amine <amine@Amines-MacBook-Pro.local>
2019-12-06 15:10:14 +0100

    Add B1 002 Vokabeln to B1 Prunfung.py

--- B1 Prüfung.py
+++ B1 Prüfung.py
@@ -3,3 +3,13 @@ B1 Prüfung
 Aufgaben 1-6 dazu -- corresponding (tabi3a laha)
 Aussagen --- al ma9oulat
 Berichtet -- you3ti ta9rir
+Die Uni - Das Ziel - Der Platz - Der Bahnhof - Das Amt
+Dabei --- baynama
+heutzutage --- hadihi al ayam
+zu bekommen -- al houssoul 3la
+nämlich -- haytou ana
+Erfolg -- naajih
+ich nahm -- akhadtou
+Fussmarsch -- machyan 3la al a9dam
+Die Übergabe -- yaoum a tasslim
+Die Zulassung -- al 9ouboul al jami3i
