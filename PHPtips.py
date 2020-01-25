2ef44dfe3a40bd140fa0c9bac8f33b9ef7f23e10
aminosninatos <christopherfgreen@hotmail.com>
2020-01-25 18:25:26 +0100

    Add single quotes vs double ones in phptips.py

---
+++ PHPtips.py
@@ -0,0 +1,6 @@
+Single vs double quotation marks
+---------------------------------
+the prefered method is to use Single quotation ''
+and using escape charcater: echo 'I\' am amine'
+when concatanation string for echo its better to use it as function
+echo 'hello', $var1, $var2 instead of echo 'hello'.$var1.$var2
