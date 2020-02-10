91f8793a9d9b2f9a7e61ec73b2ba1df5297dc971
aminosninatos <christopherfgreen@hotmail.com>
2020-02-10 20:11:25 +0100

    Add css reference to a child element to webdesign.py

---
+++ webdesign.py
@@ -0,0 +1,23 @@
+Tips
+-------
+1.Use font size between 15 and 25 pixels.
+2.For headlines and titles use really big font size.
+3.Use line spacing between 120 and 150%.
+
+Css class & ids
+-----------------
+- If u want to select a particular html element we can use classes or ids.
+- A class can be affected to as many elements we want.
+- The id can only be used once inside an html document.
+- To reference the classes in css, we use the .class_name{} notation.
+- to reference the ids in css, we use #id_name{} notation.
+- To define the style for all elements we use *{} notation.
+
+Css for an element inside a class
+----------------------------------
+- If you want to refernce a css for an element inside a class:
+we can use the notatiion: .class_name element_name {}.
+- If we can to position an element relative to a parent, we can define the position
+property of the parent as relative: parent_name{position : relative;} and then
+the position of the child as absolute: child_name{position:absolute;}.
+ 
