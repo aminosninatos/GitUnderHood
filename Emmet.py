a899208456d878d7f1707276dd8d5df7bb230eec
aminosninatos <christopherfgreen@hotmail.com>
2020-02-15 00:21:57 +0100

    Add html and css emmet shortcuts to Emmet.py

---
+++ Emmet.py
@@ -0,0 +1,26 @@
+Emmet HTML shortcut
+--------------------
+To get it work you have to press the TAB key.
+
+- to add an opening and closing tag to an element : element + TAB
+- to add the html file structure : ! + TAB
+- to create an html element with class : element.class_name + TAB
+- to add a class_name for a div element : .class_name + TAB
+- to create a form with the get method : form:get + TAB
+- to create a child item of an html element : element>child + TAB
+for example: ul>li
+- to create multiple children : element>child*number + TAB
+for example: ul>li*5
+- to create multiple sub elements inside an element: element0>element1+elment2 + TAB
+for example: div>h2+p
+- to add some lorem ipsum: p>lorem100 + TAB : will add 100 words of lorem ipsum
+- to add groups of elements : (group1 of elements) + (group2 of elements) + TAB
+for example: (div>p+img)+(div.section>p+img)
+
+
+ Emmet CSS shortcuts
+ ---------------------
+ - w10 + TAB  will create: width : 10px;
+ - h20 + TAB  will create: height :20px;
+ - p10 + TAb  will create: padding : 10px;
+ 
