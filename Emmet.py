92a94121950350ca12ab2cd18c4155e1bf42470d
aminosninatos <christopherfgreen@hotmail.com>
2020-02-16 10:08:32 +0100

    Add more emmet shortcuts to Emmet.py

--- Emmet.py
+++ Emmet.py
@@ -16,6 +16,10 @@ for example: div>h2+p
 - to add some lorem ipsum: p>lorem100 + TAB : will add 100 words of lorem ipsum
 - to add groups of elements : (group1 of elements) + (group2 of elements) + TAB
 for example: (div>p+img)+(div.section>p+img)
+- To create an element with its content: element{content} + TAB
+for example: h1{title}
+- To create an element wih multiple classes: element.class1.class2 + TAB
+for example: div.col.col-span
 
 
  Emmet CSS shortcuts
@@ -23,4 +27,9 @@ for example: (div>p+img)+(div.section>p+img)
  - w10 + TAB  will create: width : 10px;
  - h20 + TAB  will create: height :20px;
  - p10 + TAb  will create: padding : 10px;
- 
+ - c:#000 + TAB will create: color : #000
+ - t50p + TAB  will create : top : 50%;
+ - fl:r + TAB  will create : float : right;
+ - m:10 + TAB  will create : margin : 10px;
+ - mr:50 + TAB will create : margin-right : 50px;
+ - ff:a + TAB  will create : font-family : Arial;
