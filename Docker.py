45652f4c73d6763feacef8ffcf77e5668f2bdb87
aminosninatos <christopherfgreen@hotmail.com>
2020-07-07 21:35:20 +0100

    Add docker network commands to Docker.py

--- Docker.py
+++ Docker.py
@@ -48,4 +48,22 @@ will show details of the container config
 will start a NEW container interactively(t:tty i:interactive)
 
 #docker container exec -it container_id
-will start a RUN additionnal commands to an EXISTING container
+will RUN additionnal commands to an EXISTING container
+
+#docker container inspect --format '{{  .NetworkSettings.IPAddress }}' container_name
+will show the ip address of the container
+
+#docker network ls
+will show all the virtual Networks
+
+#docker network inspect network_name
+will inspect a network
+
+#docker network create --driver driver_name network_name
+will create a network
+
+#docker network connect network_name container_name
+will attach a container to a network
+
+#docker network disconnect network_name container_name
+will disconnect a container from  a network
