b65b93268461bb088f290fab7ac6328df804f0a2
Amine <amine@Amines-MacBook-Pro.local>
2020-09-29 11:42:12 +0100

    Add npm cheat sheet to Composer.py

---
+++ Npm.py
@@ -0,0 +1,9 @@
+You should execute the commands in the root directory of the project
+
+NPM commands
+------------
+> npm init : to initialize the project and create package.json file.
+> npm install :	Install everything in package.json.
+> npm uninstall package :	to remove a package.
+> npm list	: Lists the installed versions of all dependencies in this software.
+> npm update :	Update production packages
