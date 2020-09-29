eea13497e440a7175a413f09d65b97c8c8934383
Amine <amine@Amines-MacBook-Pro.local>
2020-09-29 11:35:17 +0100

    Add composer cheat sheet to Composer.py

---
+++ Composer.py
@@ -0,0 +1,13 @@
+
+Under Liunx and Mac
+---------------------
+> php composer.phar
+
+Composer commands
+------------------
+> composer init : to create composer.json  file
+> composer require vendor/package : Adds package from vendor to composer.json’s require section and installs it.
+> composer install : install all packages outlined in the composer.lock file. If the file does not exist it will look for composer.json.
+> composer update :	Updates all packages
+> composer remove vendor/package	Removes vendor/package from composer.json and uninstalls it.
+> composer create-project laravel/laravel myapp : to create laravel project.
