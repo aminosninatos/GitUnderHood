2bf34da2ec736ca0fab5d82854a89d191c001328
aminosninatos <christopherfgreen@hotmail.com>
2020-11-22 10:03:16 +0100

    Add Fortigate commands cheatsheet to Fortigate.py

---
+++ Fortigate.py
@@ -0,0 +1,18 @@
+FORTIGATE COMMANDS
+--------------------
+
+# get system status
+to view all system information.
+
+# show system interface
+to get information about interfaces.
+
+# diag sys top
+to view all processes running.
+
+# config system interface
+# edit port1
+# set mode static
+# set ip address x.x.x.x x.x.x.x
+# show system interface
+to set a static ip address for interface port1.
