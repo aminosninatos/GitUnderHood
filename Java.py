108e2ec1ca8f00a4cd2d78197f2e1014f220a06b
aminosninatos <christopherfgreen@hotmail.com>
2020-12-29 19:35:07 +0100

    Add access modificators to Java.py

--- Java.py
+++ Java.py
@@ -8,3 +8,9 @@ Step 1:
 javac + Filename.java
 Step 2:
 java + Filename // execute without add .java
+
+Access Modificators
+--------------------
+For class only two : public : the class is visible everywhere, defaut : class visible only in the same package.
+For variables four : public : visible everywhere, private: visible only in the same class, proctected : visible
+in the same package and visibile for the child classes, defaut : visible only in the same package.
