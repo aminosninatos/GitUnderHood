f8eebcdd88b1523b354d8113a7f3336e74bb3593
aminosninatos <christopherfgreen@hotmail.com>
2021-01-04 20:01:48 +0100

    Add encapsulation definition to Java.py

--- Java.py
+++ Java.py
@@ -14,3 +14,9 @@ Access Modificators
 For class only two : public : the class is visible everywhere, defaut : class visible only in the same package.
 For variables four : public : visible everywhere, private: visible only in the same class, proctected : visible
 in the same package and visibile for the child classes, defaut : visible only in the same package.
+
+Encapsulation
+-------------
+Encapsulation in Java is a mechanism of wrapping the data (variables) and code acting on the data (methods)
+together as a single unit. In encapsulation, the variables of a class will be hidden from other classes, and
+can be accessed only through the methods of their current class.
