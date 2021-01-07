b5a909c2e14297bae299089cea0db293d2ba849e
aminosninatos <christopherfgreen@hotmail.com>
2021-01-07 19:54:48 +0100

    Add java collections definition to Java.py

--- Java.py
+++ Java.py
@@ -40,3 +40,11 @@ To access the interface methods, the interface must be "implemented" (kinda like
 class with the implements keyword (instead of extends).
 Like abstract classes, interfaces cannot be used to create objects (in the example above, it is not possible
 to create an "Animal" object in the MyMainClass)
+
+Collections
+-----------
+The Collection in Java is a framework that provides an architecture to store and manipulate the group of objects.
+Java Collections can achieve all the operations that you perform on a data such as searching, sorting, insertion,
+manipulation, and deletion.
+Java Collection means a single unit of objects. Java Collection framework provides many interfaces (Set, List,
+Queue, Deque) and classes (ArrayList, Vector, LinkedList, PriorityQueue, HashSet, LinkedHashSet, TreeSet).
